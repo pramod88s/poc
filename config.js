946616f8dcc5c7171a536c815ea5ee3fb0730a57
pramod88s <pramod.somvanshi@gmail.com>
2017-06-13 15:42:14 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AAAAFP25cNQ:APA91bGGNy5HLAFw3aas-fXMApZ4VI83imKO_pp9kEmcye52ggtK75uM9tO-_4W36x9Cu1b8I8nzFul-gvJdgCPO2t4228_g-XsuZaBhh_0ILlSgbxTFNs6MDF7motgyOXYhEu_b7SmU'
+};
